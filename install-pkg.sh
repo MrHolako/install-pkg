73b1ee76bde4c79867f2648a546ea054f7852b08
Mr.Holakk <45064556+MrHolako@users.noreply.github.com>
2018-11-15 12:15:08 +0200

    add

---
+++ install-pkg.sh
@@ -0,0 +1,199 @@
+#!bin/bash
+
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+clear
+echo
+echo
+echo
+echo
+echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo -e $green
+figlet install pkg
+echo
+echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo
+echo -e $cyan "Author        : Mr.Holako"
+echo -e $cyan "Telegram  : t.me/HolakoSyria963"
+echo -e $cyan "Twitter       : https://twitter.com/holako_sy_963"
+echo -e $white
+pkg install python
+y
+pkg install python2
+y
+pkg install git
+y
+pkg install nano
+y
+pkg install figlet
+y
+pkg install wget
+y
+pkg install ruby
+y
+apt update
+y
+apt upgrade
+y
+apt-get update
+y
+apt-get upgrade
+y
+apt install php
+y
+apt install python
+y
+apt install python2
+y
+apt install git
+y
+apt install golang
+y
+apt install host 
+y
+apt install nano
+y
+apt install havij 
+y
+apt install hydra
+y
+apt install wireshark 
+y
+apt install cmatrix
+y
+pkg install figlet
+y
+pkg install wget
+y
+pkg install wget -y
+y
+pkg install python2 -y
+y
+pkg install python2-dev -y
+y
+apt install wireshark 
+y
+pkg install cowsay
+y
+pkg install toilet
+y
+pkg install ruby
+y
+pkg install help 
+y
+gem install lolcat
+y
+pkg install curl
+y
+pkg install wgetrc 
+y
+pkg install unzip
+y
+pkg install openssh
+y
+pkg install tor
+y
+pkg install tar
+y
+pkg install zip
+y
+pkg install net-tools
+y
+pkg install unrar
+
+pkg install clang
+y
+pkg install w3m
+y
+pkg install proot
+y
+pip2 install wget
+y
+pip2 install requests
+y
+apt update
+y
+apt upgrade
+y
+apt install git
+y
+apt install Python
+y
+apt install Python2
+y
+apt installe Python3
+y
+apt install nano
+y
+apt install Python pipeapt install ruby
+y
+pip install bundle
+y
+apt-get install gcc
+y
+apt-get install Build- essential
+y
+apt-get install perl
+y
+apt-get install git
+y
+pkg update
+y
+pkg upgrade
+y
+apt update && apt upgrade -y
+y
+apt install git
+y
+apt install python3
+y
+apt install python
+y
+apt install ruby
+y
+apt install wget
+y
+apt install unzip
+y
+apt install zip
+y
+apt install nano
+y
+apt install perl
+y
+apt install curl
+y
+apt install proot
+y
+apt update && apt upgrade 
+y
+pkg install git python2 php curl
+y
+pkg install grep 
+y
+apt-get install python3
+y
+pip3 install requests bs4
+y
+apt-get install git
+y
+pkg install git python python2 unzip nano
+y
+pip install requestes
+y
+get update
+y
+apt-get install python 
+y
+apt-get install python
+y
+apt-get install git
+y
+echo -e $green
+figlet Successfully Completed
+echo -e white
